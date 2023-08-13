55481898cfdd0ed829d4197ec50f0883c8af2fcf
amynee <tlijaniamine9@gmail.com>
2023-08-13 11:24:51 +0200

    transactions page

---
+++ src/app/pages/transactions/transactions.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-transactions',
+  templateUrl: './transactions.component.html',
+  styleUrls: ['./transactions.component.scss']
+})
+export class TransactionsComponent {
+
+  constructor(
+    private router: Router
+  ) {
+  }
+
+  newTransaction() {
+    this.router.navigate(['transactions', 'new-transaction']);
+  }
+}
